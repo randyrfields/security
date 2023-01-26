5f956b51c76b307ef7c4e954897d8276e6691ba6
Randy Fields <randyrfields@gmail.com>
2023-01-26 07:46:29 -0500

    initial commit

---
+++ SecureSite/SecureSite/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
